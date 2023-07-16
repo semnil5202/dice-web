85933d96b2c3c7149d559c114c8f0d417decf8cf
semnil5202 <semnil5202@gmail.com>
2023-07-17 04:10:45 +0900

    refactor: 이미지와 텍스트 싱크 방법 변경

--- main.js
+++ main.js
@@ -4,45 +4,37 @@ const startingGameBtn = document.querySelector('button.starting-game');
 const diceImg = document.querySelector('img');
 const result = document.querySelector('h2');
 
-const isKakao = Boolean(navigator.userAgent.match('KAKAO'));
-
 let mode = 6;
-const syncs =
-  (!isKakao && JSON.parse(localStorage.getItem('textDelay'))) ||
-  new Array(12).fill(false);
 
 diceModeSixBtn.addEventListener('click', () => {
   mode = 6;
   diceImg.src = './diceInit.svg';
-  result.innerHTML = '최대값이 6으로 설정되었습니다.';
+
+  diceModeSixBtn.classList.add('active');
+  diceModeTwelveBtn.classList.remove('active');
 });
 
 diceModeTwelveBtn.addEventListener('click', () => {
   mode = 12;
   diceImg.src = './diceInit.svg';
-  result.innerHTML = '최대값이 12으로 설정되었습니다.';
+
+  diceModeSixBtn.classList.remove('active');
+  diceModeTwelveBtn.classList.add('active');
 });
 
 startingGameBtn.addEventListener('click', () => {
   const diceNumber = Math.floor(Math.random() * mode + 1);
-  let textDelay = 750;
 
   diceImg.src = `./diceInit.svg`;
-  result.innerHTML = '';
 
   const diceTimerId = setTimeout(() => {
     diceImg.src = `./dice${diceNumber}.svg`;
     clearTimeout(diceTimerId);
   }, 500);
+});
 
-  if (syncs[diceNumber - 1]) textDelay = 500;
-
-  const resultTimerId = setTimeout(() => {
-    result.innerHTML = diceNumber;
-    clearTimeout(resultTimerId);
-  }, textDelay);
-
-  syncs[diceNumber - 1] = true;
-
-  !isKakao && localStorage.setItem('textDelay', JSON.stringify(syncs));
+diceImg.addEventListener('load', (e) => {
+  result.innerHTML = '';
+  const diceInfos = e.target.src.match(/([0-9]+)\.svg$/);
+  if (diceInfos) result.innerHTML = diceInfos[1];
 });
