9a524e19a34649bd60962ba74f41f16b373cc28a
semnil5202 <semnil5202@gmail.com>
2023-07-17 01:56:19 +0900

    feat: 기능구현완료

---
+++ main.js
@@ -0,0 +1,33 @@
+const diceModeSixBtn = document.querySelector('button.mode-six');
+const diceModeTwelveBtn = document.querySelector('button.mode-twelve');
+const startingGame = document.querySelector('button.starting-game');
+const diceImg = document.querySelector('img');
+const result = document.querySelector('h2');
+
+let mode = 6;
+
+diceModeSixBtn.addEventListener('click', () => {
+  mode = 6;
+  diceImg.src = './juinit.png';
+  result.innerHTML = '최대값이 6으로 설정되었습니다.';
+});
+
+diceModeTwelveBtn.addEventListener('click', () => {
+  mode = 12;
+  diceImg.src = './juinit.png';
+  result.innerHTML = '최대값이 12으로 설정되었습니다.';
+});
+
+startingGame.addEventListener('click', () => {
+  const diceNumber = Math.floor(Math.random() * mode + 1);
+  const animNumber = Math.floor(Math.random() * mode + 1);
+
+  diceImg.src = `./juinit.png`;
+  result.innerHTML = '';
+
+  const timerId = setTimeout(() => {
+    diceImg.src = `./ju${diceNumber}.png`;
+    result.innerHTML = diceNumber;
+    clearTimeout(timerId);
+  }, 500);
+});
